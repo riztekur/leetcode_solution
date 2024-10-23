7c6e56912958ab2c8d33eaa4aa859a00d86bf11a
riztekur <60560427+riztekur@users.noreply.github.com>
2024-10-23 19:25:47 +0700

    revise 433

--- LeetCode 75/lc_443.py
+++ LeetCode 75/lc_443.py
@@ -1,19 +1,19 @@
 class Solution:
   def compress(self, chars: List[str]) -> int:
-    ans = 0
+    length = 0
     i = 0
 
     while i < len(chars):
-      letter = chars[i]
-      count = 0
-      while i < len(chars) and chars[i] == letter:
-        count += 1
+      c = chars[i]
+      counter = 0
+      while i < len(chars) and chars[i] == c:
+        counter += 1
         i += 1
-      chars[ans] = letter
-      ans += 1
-      if count > 1:
-        for c in str(count):
-          chars[ans] = c
-          ans += 1
+      chars[length] = c
+      length += 1
+      if counter > 1:
+        for c in str(counter):
+          chars[length] = c
+          length += 1
 
-    return ans
+    return length
